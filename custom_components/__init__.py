d9af0f3750fd4d99458907bbe368fa8127bbaad6
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2024-04-23 07:25:31 +0200

    Add files via upload

---
+++ custom_components/__init__.py
@@ -0,0 +1,28 @@
+"""The RCE component."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+
+from .const import DOMAIN
+
+PLATFORMS = [Platform.CALENDAR, Platform.SENSOR]
+
+async def async_setup(hass: HomeAssistant, config: dict):
+     """Set up the component."""
+     hass.data[DOMAIN] = {}
+     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+     """Set up RCE integration."""
+     hass.async_create_task(
+          hass.config_entries.async_forward_entry_setup(entry, "rce")
+     )
+     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+     """Unload a config entry."""
+     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+          hass.data[DOMAIN].pop(entry.entry_id)
+
+     return unload_ok
