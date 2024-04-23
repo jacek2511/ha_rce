0bdbc39a4457d707a9e5cdf8c2916e1e7570edb5
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2024-04-23 22:51:38 +0200

    Update sensor.py

--- custom_components/rce/sensor.py
+++ custom_components/rce/sensor.py
@@ -23,54 +23,82 @@ async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entitie
     async_add_entities([RCESensor()])
 
 
-class RCECalendar(CalendarEntity):
-    """Representation of a Sensor."""
-    
+class RCESensor(SensorEntity):
+    "Sensors data"
+    _attr_device_class = SensorDeviceClass.MONETARY
+    _attr_suggested_display_precision = None
+    _attr_state_class = SensorStateClass.TOTAL    
 
     def __init__(self) -> None:
-        _LOGGER.info("RCE calendar")
+        _LOGGER.info("RCE sensor")
         super().__init__()
-        self.ev = []
         self.cr_time = None
         self.last_update = None
         self.cloud_response = None
         self.last_network_pull = datetime(
             year=2000, month=1, day=1, tzinfo=timezone.utc
         )
-        self._attr_unique_id = "rce_calendar" 
-        self._attr_name = "RCE calendar"
 
+        self._hass = hass
+        self._attr_force_update = True
 
-    async def async_get_events(
-        self,
-        hass: HomeAssistant,
-        start_date: datetime.datetime,
-        end_date: datetime.datetime,
-    ) -> list[CalendarEvent]:
-        """Return calendar events within a datetime range."""
-        ret = []
-        ev: CalendarEvent
-        for ev in self.ev:
-            if (
-                start_date < ev.start
-                and ev.start < end_date
-                or start_date < ev.start
-                and ev.end < end_date
-                or start_date < ev.end
-                and ev.end < end_date
-            ):
-                ret.append(ev)
-        return ret
+        # Price by current hour.
+        self._current_price = None
+
+        # Holds the data for today and morrow.
+        self._data_today = SENTINEL
+        self._data_tomorrow = SENTINEL
+
+        # To control the updates.
+        self._last_tick = None
 
     @property
-    def event(self) -> CalendarEvent | None:
-        """Return the next upcoming event."""
+    def name(self) -> str:
+        return self.unique_id
 
-        ev: CalendarEvent
-        for ev in self.ev:
-            if datetime.now(ZoneInfo(self.hass.config.time_zone)) < ev.end:
-                return ev
+    @property
+    def should_poll(self):
+        """No need to poll. Coordinator notifies entity of updates."""
+        return False
 
+    @property
+    def icon(self) -> str:
+        return "mdi:flash"
+
+    @property
+    def unit(self) -> str:
+        """Unit"""
+        return self._price_type
+
+    @property
+    def unit_of_measurement(self) -> str:  # FIXME
+        """Return the unit of measurement this sensor expresses itself in."""
+        _currency = self._currency
+        if self._use_cents is True:
+            # Convert unit of measurement to cents based on chosen currency
+            _currency = _CURRENTY_TO_CENTS[_currency]
+        return "%s/%s" % (_currency, self._price_type)
+
+    @property
+    def unique_id(self):
+        name = "rce_%s_%s_%s_%s" % (
+            self._price_type,
+            self._area,
+            self._currency,
+            self._precision,
+        )
+        name = name.lower().replace(".", "")
+        return name
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "name": self.name,
+            "manufacturer": DOMAIN,
+        }
+  
+    @property
     def today(self):
         """fetch today data"""
         now = datetime.now(ZoneInfo(self.hass.config.time_zone))
@@ -84,6 +112,7 @@ class RCECalendar(CalendarEntity):
         except ReadTimeout:
             self.cloud_response = ""
 
+    @property
     def tomorrow(self):
         """fetch tomorrow data"""
         now = datetime.now(ZoneInfo(self.hass.config.time_zone)) + timedelta(days=1)
@@ -96,45 +125,3 @@ class RCECalendar(CalendarEntity):
         except requests.exceptions.ReadTimeout:
             self.cloud_response = ""
 
-    def csv_to_events(self, csv_reader: csv, day: datetime):
-        """Transform csv to events"""
-        
-        for row in csv_reader:
-            if not row[1].isnumeric():
-                continue
-            self.ev.append(
-                CalendarEvent(
-                    day.replace(hour=int(row[1])-1),
-                    day.replace(hour=int(row[1])-1,minute=59,second=59),
-                    row[2],
-                    description="https://www.pse.pl/dane-systemowe/funkcjonowanie-rb/raporty-dobowe-z-funkcjonowania-rb/podstawowe-wskazniki-cenowe-i-kosztowe/rynkowa-cena-energii-elektrycznej-rce",
-                )
-            )
-            event_start = int(row[1])
-
-    async def async_update(self):
-        """Retrieve latest state."""
-        now = datetime.now(ZoneInfo(self.hass.config.time_zone))
-        if now < self.last_network_pull + timedelta(minutes=30):
-            return
-        self.last_network_pull = now
-        self.cloud_response = None
-        await self.hass.async_add_executor_job(self.today)
-
-        if self.cloud_response is None or self.cloud_response.status_code != 200:
-            return False
-        self.ev.clear()
-
-        csv_output = csv.reader(self.cloud_response.text.splitlines(), delimiter=";")
-        now = now.replace(minute=0).replace(second=0)
-        self.csv_to_events(csv_output, now)
-
-        self.cloud_response = None
-        await self.hass.async_add_executor_job(self.tomorrow)
-
-        if self.cloud_response is None or self.cloud_response.status_code != 200:
-            return False
-
-        csv_output = csv.reader(self.cloud_response.text.splitlines(), delimiter=";")
-        now = now.replace(minute=0).replace(second=0) + timedelta(days=1)
-        self.csv_to_events(csv_output, now)
