ed16bfb78fa2b35eb9c9af083924ee507e2f46e6
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2024-05-04 23:46:08 +0200

    Update __init__.py

--- custom_components/rce/__init__.py
+++ custom_components/rce/__init__.py
@@ -2,14 +2,14 @@
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
+#from homeassistant.helpers.typing import ConfigType
 from homeassistant.const import Platform
 
 from .const import DOMAIN
 
 PLATFORMS = [Platform.SENSOR]
 
-async def async_setup(hass: HomeAssistant, config: configType) -> bool:
+async def async_setup(hass: HomeAssistant, config) -> bool:
      """Set up the component."""
      hass.data[DOMAIN] = {}
      return True
@@ -17,19 +17,15 @@ async def async_setup(hass: HomeAssistant, config: configType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
      """Set up RCE integration."""
      hass.async_create_task(
-          hass.config_entries.async_forward_entry_setup(entry, DOMAIN)
+          hass.config_entries.async_forward_entry_setup(entry, "sensor")
      )
      return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
      """Unload a config entry."""
-     unload_ok = await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
-     
-     if unload_ok:
+     if unload_ok := await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS):
           hass.data[DOMAIN].pop(entry.entry_id)
           return unload_ok
-     return False
-     
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
      """Reload config entry."""
