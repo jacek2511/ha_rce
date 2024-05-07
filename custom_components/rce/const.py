3a60f06f6769d33caa80f68c68d6791cd1618a74
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2024-05-07 23:28:19 +0200

    Update const.py

--- custom_components/rce/const.py
+++ custom_components/rce/const.py
@@ -1,9 +1,11 @@
 """Constants for the RCE integration."""
 
 from datetime import timedelta
+import logging
 
 DOMAIN = "rce"
 DEFAULT_CURRENCY = "zł"
 DEFAULT_PRICE_TYPE = "MWh"
+_LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(seconds=30)
