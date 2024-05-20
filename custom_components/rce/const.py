21d10583831af5c5d2fa5d66cd63e49494941902
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2024-05-20 17:51:34 +0200

    Update const.py

--- custom_components/rce/const.py
+++ custom_components/rce/const.py
@@ -1,11 +1,20 @@
 """Constants for the RCE integration."""
 
 from datetime import timedelta
+from typing import Final
+
 import logging
 
-DOMAIN = "rce"
-DEFAULT_CURRENCY = "zł"
-DEFAULT_PRICE_TYPE = "MWh"
+DOMAIN: Final = "rce"
+DEFAULT_CURRENCY: Final = "PLN"
+DEFAULT_PRICE_TYPE: Final = "MWh"
+
+DEFAULT_CUSTOM_PEAK_HOURS_RANGE = "10-17"
+DEFAULT_LOW_PRICE_CUTOFF = 90
+
+CONF_CUSTOM_PEAK_HOURS_RANGE: Final = "custom_peak_range"
+CONF_LOW_PRICE_CUTOFF: Final = "low_price_cutoff"
+
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(seconds=30)
