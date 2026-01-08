53166b173e676bf6f181d4ea69a0ebdca87c3361
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2026-01-08 13:52:07 +0100

    Update __init__.py

--- custom_components/rce/__init__.py
+++ custom_components/rce/__init__.py
@@ -2,10 +2,14 @@
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.const import Platform
 
 from .const import DOMAIN, _LOGGER
 
-async def async_setup(hass: HomeAssistant, config: dict):
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up this integration using YAML is not supported."""
     if DOMAIN not in hass.data:
         hass.data.setdefault(DOMAIN, {})
@@ -15,20 +19,25 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up RCE integration."""
     _LOGGER.info("RCE-async_setup_entry " + str(entry))
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
+    """Unload config entry."""
     _LOGGER.info("RCE-async_unload_entry remove entities")
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    )
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
+    if unload_ok:
+        if DOMAIN in hass.data:
+            for unsub in hass.data[DOMAIN].listeners:
+                unsub()
+        hass.data.pop(DOMAIN)
+
+        return True
+
+    return False
+    
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-     """Reload config entry."""
-     await async_unload_entry(hass, entry)
-     await async_setup_entry(hass, entry)
+    """Reload config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
